cc201c6263315801760e765049f53dc08ab6cdbc
apena415 <16394931+apena415@users.noreply.github.com>
2020-07-30 13:47:01 -0700

    added local storage of cart state  using redux-persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,21 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+
+// Grab localstorage from window object
+import storage from 'redux-persist/lib/storage';
+
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'] /* only reducer we want to persist is cart, user is handled by google auth */
+}
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
