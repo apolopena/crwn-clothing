0001b21e56d3329a2f3451d5d7a24d2b2518a70b
apena415 <16394931+apena415@users.noreply.github.com>
2020-08-01 10:22:40 -0700

    conditionally added logger for development only

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,11 @@ import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === 'development'){
+  middlewares.push(logger);
+}
 
 const store = createStore(
   rootReducer, 
