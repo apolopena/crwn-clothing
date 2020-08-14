60df644f51036bd6440426e59c93029a621b61a9
apena415 <16394931+apena415@users.noreply.github.com>
2020-08-14 13:14:21 -0700

    fixed bug for stripe payment error: alert does not show up

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -15,9 +15,9 @@ const StripeCheckoutButton = ({ price }) => {
         token
       }
     }).then(response => {
-      alert('Payment successful')
+      alert('Payment successful');
     }).catch(error => {
-      console.log('Payment error: ', JSON.parse(error));
+      console.log('Payment error: ', error);
       alert(
         'There was an issue with your payment. Please use the test payment information given on the page.'
       );
