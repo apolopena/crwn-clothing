fa7ed4d35560a26f25080ed32fce4358eea7f826
apena415 <16394931+apena415@users.noreply.github.com>
2020-08-14 12:43:44 -0700

    seperated cra app to client folder, added express server to handle stripe payments, end of section 22

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import  StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
-const onToken = token => {
-  console.log(token);
-  alert('Payment Successful')
-}
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_51HB3rqJGt8gGJLdfyrpCi4msuK2gj8mbq5ZRCW8NsGASA9Hc6kpFj5moJFUvmTsMagwYxE4YmeEDENFYZyXnlpaQ00cb3MqJCk';
+  
+  const onToken = token => {
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    }).then(response => {
+      alert('Payment successful')
+    }).catch(error => {
+      console.log('Payment error: ', JSON.parse(error));
+      alert(
+        'There was an issue with your payment. Please use the test payment information given on the page.'
+      );
+    });
+  }
+  
   return (
     <StripeCheckout
       label='Pay Now'
